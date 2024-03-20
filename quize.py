380d3765810c24adc6613befb6756c545de3f451
Pallavi_addala <151432100+Pallaviaddal@users.noreply.github.com>
2024-03-20 18:28:09 +0530

    Add files via upload

---
+++ quize.py
@@ -0,0 +1,32 @@
+print("Welcome come to quiz program...!")
+num=input("Are you interested in quiz ?")
+print(num)
+if num=="yes":
+    print("yes i'm interested!")
+else:
+    print("no!")
+print("Now we will start the quiz program")
+num1=input("What is the maximum possible length of an identifier?")
+print(num1)
+if num1=="no fixed length":
+    print("correct!")
+else:
+    print("incorrect!")
+num2=input("What is correct syntax to output Hello World in python?")
+print(num2)
+if num2 =='print("Hello World")':
+    print("Correct!")
+else:
+    print("Incorrect!")
+num3=input("How do you insert COMMENTS in python code?")
+print(num3)
+if num3=="#COMMENTS":
+    print("Correct!")
+else:
+    print("Incorrect!")
+num4=input("Which one is NOT a legal variable name?")
+print(num4)
+if num4=="my_var":
+    print("correct!")
+else:
+    print("Incorrect")
